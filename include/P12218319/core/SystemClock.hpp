54a3d08d9ec46ba5ffc4184215e1637e8c7088f2
SolaireLibrary <solairelibrary@mail.com>
2016-02-26 18:33:38 +0000

    Added functions for system time

---
+++ include/P12218319/core/SystemClock.hpp
@@ -0,0 +1,28 @@
+/*
+Copyright 2016 Adam Smith - P12218319
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+email : p12218319@myemail.dmu.ac.uk
+*/
+#ifndef P12218319_CORE_SYSTEM_CLOCK_HPP
+#define P12218319_CORE_SYSTEM_CLOCK_HPP
+
+#include "P12218319\core\Core.hpp"
+
+namespace P12218319 { namespace systemClock {
+	P12218319_EXPORT_API uint64_t P12218319_CALL Milliseconds();
+	P12218319_EXPORT_API uint64_t P12218319_CALL Microseconds();
+	P12218319_EXPORT_API uint64_t P12218319_CALL Nanoseconds();
+}}
+#endif
